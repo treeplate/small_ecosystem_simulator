3e7b3e6d967ff0827ecdb3925144c763a62faa9f
Tree Plate <tree.plate.moo@gmail.com>
2020-06-23 21:06:43 +0000

    added ticking tests

--- test.dart
+++ test.dart
@@ -5,4 +5,11 @@ void main() {
     expect(TestAnimal().canEat(TestPlant()), equals(true));
     expect(TestAnimal().canEat(TestAnimal()), equals(false));
   });
+  var times = 0;
+  var world = World();
+  Plan plan = Plan(() => times++);
+  test("World.tick ticks"), () {
+    world.tick(plan);
+    expect(times == 1);
+  });
 }
