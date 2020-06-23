08cdd4a3a55d8e142880607578e6bfd28c28fca5
treeplate <51892395+treeplate@users.noreply.github.com>
2020-06-23 11:17:08 -0700

    Create test.dart

---
+++ test.dart
@@ -0,0 +1,8 @@
+import 'package:test/test.dart'
+import 'beings.dart';
+void main() {
+  test("canEat works", () {
+    expect(TestAnimal().canEat(TestPlant()), equals(true));
+    expect(TestAnimal().canEat(TestAnimal()), equals(false));
+  });
+}
